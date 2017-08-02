85307842bc6d64b3b74ba9f3f2926c00379c6e88
wegga <wegga@DESKTOP-87CIQ4O>
2017-08-02 06:21:28 -0300

    x

---
+++ src/Controle/BancoDados.java
@@ -0,0 +1,375 @@
+package Controle;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import App.App;
+import Model.Cliente;
+import Model.Empregado;
+import Model.Pedido;
+import Model.Produtos;
+import Model.Seccao;
+import Model.Venda;
+
+public class BancoDados {
+
+	private Connection connection = null;
+	private Statement statement = null;
+	private ResultSet resultSet;
+	private ResultSet resultSetDois =  null;
+	
+	public void conectar(){
+		String servidor = "jdbc:mysql://localhost:3306/fbd_grafica?useSSL=false";
+		String usuario = "root";
+		String senha ="1234";
+		String drive ="com.mysql.jdbc.Driver";
+		
+		try {
+			Class.forName(drive);
+			this.connection = DriverManager.getConnection(servidor, usuario, senha);
+			this.statement = this.connection.createStatement();
+		} catch (Exception e) {
+			System.out.println("erro ao conec "+e);
+			JOptionPane.showMessageDialog(null, "Erro ao gravar /n"+e);
+		}
+	}	
+
+	public boolean estaConectado(){
+	
+		if(this.connection != null){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public void gravarCliente(Cliente cliente){
+		
+		try {
+			 String query= "insert into cliente (nome,tipo,telefone) values('"+cliente.getNome()+"',"+cliente.getTipo()+",'"+cliente.getTelefone()+"');";
+			 this.statement.executeUpdate(query);
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void gravarSeccao(Seccao sec){
+		
+		String query = "insert into seccao (nome,descricao) values('"+sec.getNome()+"',"+sec.getDescricao()+"');";
+		try {
+				 this.statement.executeUpdate(query);
+		} catch (SQLException e) {
+				System.out.println(e);
+		}
+		query  = null;
+		
+	}	
+		
+	public void gravarProdutos(Produtos prod){
+		
+		String query = "insert into Produto (nome,cod_Barras,validade,preço,quant_estoque,descrição,id_seccao) "
+				+ "values('"+prod.getNome()+"','"+prod.getCodBarras()+"','"+prod.getDataValidade()+"','"+prod.getPreco()+"','"+prod.getQuantEstoque()+"','"+prod.getDescricao()+"','"+prod.getIdSeccao()+"')";
+		try {
+				 this.statement.executeUpdate(query);
+		} catch (SQLException e) {
+				System.out.println(e);
+		}
+		query  = null;
+		
+	}	
+
+	public void gravarEmpregado(Empregado emp){
+		try {
+			 String query = "insert into empregado (nome,tipo,telefone,matricula,senha) values('"+emp.getNome()+"',"+emp.getTipo()+",'"+emp.getTelefone()+"','"+emp.getMatricula()+"','"+emp.getSenha()+");";
+			 this.statement.executeUpdate(query);
+			} catch (SQLException e) {
+				System.out.println(e);
+			}
+	}
+	
+	public void gravarPedido(Pedido ped){
+		try {
+			 String query = "insert into pedido(valor-total,id_Cliente) values('"+ped.getValorTotal()+"','"+ped.getIdCliente()+"');";
+			 this.statement.executeUpdate(query);
+			} catch (SQLException e) {
+				System.out.println(e);
+			}
+	}
+
+	public void gravarContemEmPedido(int idProduto, int idPedido, int quantidade){
+		try {
+			 String query = "insert into Contem_pedido(id-pedido,id-produto,quantidade) values('"+idPedido+"','"+idProduto+"','"+quantidade+"');";
+			 this.statement.executeUpdate(query);
+			} catch (SQLException e) {
+				System.out.println(e);
+			}
+	}
+	
+	public void gravarVenda(Venda venda){
+		try {
+			 String query = "insert into Venda(Forma_pagamento,data,id_Empregado,id_Pedido) values('"+venda.getFormaPagamento()+"','"+venda.getData()+"','"+venda.getIdEmpregado()+"','"+venda.getPedido().getId()+"');";
+			 this.statement.executeUpdate(query);
+			} catch (SQLException e) {
+				System.out.println(e);
+			}
+	}
+
+	public ArrayList<Cliente> pesquisarCliente(String nome){
+		
+		ArrayList<Cliente > clientes =  new ArrayList<>();
+		try {
+			String query = "select * from cliente where nome ='"+nome+"';";
+			this.resultSet = this.statement.executeQuery(query);
+			while (this.resultSet.next()){
+				clientes.add(new Cliente(
+						this.resultSet.getInt("id"),
+						this.resultSet.getString("nome"), 
+						this.resultSet.getInt("tipo"), 
+						this.resultSet.getString("telefone")));
+			}
+			
+			return clientes;
+			
+		} catch (SQLException e) {
+			System.out.println("erro : "+e);
+			e.printStackTrace();
+		}
+		return null;	
+	}
+	
+	public Cliente pesquisarClienteId(int id){
+		
+		ArrayList<Cliente > clientes =  new ArrayList<>();
+		try {
+			String query = "select * from cliente where id='"+id+"'";
+			statement= connection.prepareStatement(query);
+			resultSet = statement.executeQuery(query);
+			
+			this.resultSet.next();
+			
+			int i = resultSet.getInt("id");
+			
+			
+			Cliente c= new Cliente(
+						i,
+						resultSet.getString("nome"), 
+						resultSet.getInt("tipo"), 
+						resultSet.getString("telefone"));
+			 System.out.println(c.getNome());
+			 
+			 return c;
+			 
+		} catch (SQLException e) {
+			System.out.println("erro : "+e);
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+		return null;	
+	}
+	
+	
+	public ArrayList<Produtos> pesquisarProd(String nome){
+		
+		
+		try {
+			ArrayList<Produtos> produtos =  new ArrayList<>();
+			String query = "select * from produto where nome ='"+nome+"';";
+			this.resultSet = this.statement.executeQuery(query);
+			while(this.resultSet.next()){	
+				produtos.add(new Produtos(
+						this.resultSet.getInt("id"),
+						this.resultSet.getString("nome"),
+						this.resultSet.getNString("cod_barras"),
+						this.resultSet.getString("validade"), 
+						this.resultSet.getFloat("preço"), 
+						this.resultSet.getInt("quant_estoque"),
+						this.resultSet.getString("descrição"),
+						this.resultSet.getInt("id_seccao")));
+			}
+			
+			return produtos;
+		} catch (SQLException e) {
+			System.err.println("Erro : "+e);
+			
+		}
+		
+		
+		return null;
+	}
+
+	public Produtos pesquisarProdCodigo(int cod_barras){
+		
+		
+		try {
+			
+			String query = "select * from produto where cor_barras ='"+cod_barras+"';";
+			this.resultSet = this.statement.executeQuery(query);
+			return new Produtos(
+						this.resultSet.getInt("id"),
+						this.resultSet.getString("nome"),
+						this.resultSet.getNString("cod_barras"),
+						this.resultSet.getString("validade"), 
+						this.resultSet.getFloat("preço"), 
+						this.resultSet.getInt("quant_estoque"),
+						this.resultSet.getString("descrição"),
+						this.resultSet.getInt("id_seccao"));
+			
+		} catch (SQLException e) {
+			System.err.println("Erro : "+e);
+			
+		}
+		
+		
+		return null;
+		
+	}
+	
+	public void retirarDoEstoque(String cod_barras){
+		
+		String query = "update produto set quant_estoque = quant_estoque-1 where cod_barras = "+cod_barras+";";
+		
+		try {
+			this.statement.executeQuery(query);
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public Pedido consultarPedido(int idPedido){
+		
+		Pedido pedido = new Pedido() ;
+		pedido.setId(idPedido);
+		
+		try {
+			String query =  "select *  from produto  as prod inner join pedido as ped inner join contem_pedido as cont on  cont.id_Pedido=ped.id  and  cont.id_produto=prod.id  where ped.id = '"+idPedido+"'; ";
+			this.resultSetDois = this.statement.executeQuery(query);
+			
+			while(resultSetDois.next()){
+				pedido.getProdNoCarrinho().add(App.banco.pesquisarProdCodigo(resultSetDois.getInt("cod_barras")));
+				pedido.getQuantDeProd().add(resultSetDois.getInt("quantidade"));
+				pedido.setValorTotal(pedido.getValorTotal()+(this.resultSetDois.getFloat("preço")*this.resultSetDois.getInt("quantidade")));
+			}
+			
+			return pedido;
+		} catch (SQLException e) {
+			System.err.println("Erro : "+e);
+			
+		}
+		
+		return null;
+	}
+	
+	public ArrayList<Venda> listarVendas(String data){
+		
+		ArrayList<Venda> vendas  =  new ArrayList<Venda>();
+		
+		try {
+			String query =  "select *  from venda  as vend inner join pedido as ped inner join contem_pedido as cont on  vend.id_Pedido=ped.id  and  cont.id_pedido=ped.id  where vend.data = '"+data+"'; ";
+			this.resultSet = this.statement.executeQuery(query);
+			
+			
+			while(this.resultSet.next()){
+				vendas.add( new Venda(this.resultSet.getInt("id"),
+						new Pedido(this.resultSet.getInt(6), this.resultSet.getFloat("valor_total"), this.resultSet.getInt("id_cliente")), 
+						this.resultSet.getString("data"), 
+						this.resultSet.getInt("id_Empregado"), 
+						this.resultSet.getInt("Forma_pagamento")));
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		return null;
+	}
+
+	public ArrayList<Seccao> pesquisarSeccao(String nome){
+		
+		ArrayList<Seccao> seccao = new ArrayList<>();
+		String query =  "select *  from  seccao where nome = '%"+nome+"%';";
+		try {
+			this.resultSet = this.statement.executeQuery(query);
+			
+			while(this.resultSet.next()){
+				seccao.add(new Seccao(
+						
+						this.resultSet.getInt("id"),
+						this.resultSet.getString("nome"),
+						this.resultSet.getString("descricao")));
+			}
+		
+			return seccao;
+			
+		} catch (SQLException e) {
+			System.err.println("Erro :  "+e);
+			
+		}
+		
+		return null;
+	}
+
+	public boolean validarLogin(String login, String senha){
+		
+	
+		try {
+			String query =  "select senha  from  empregado  where  nome = '"+login+"' ;";
+			this.resultSet =  this.statement.executeQuery(query);
+			if(!resultSet.next()){
+				return false;
+			}
+			String s = this.resultSet.getString("senha");
+			if(s.equals(senha)){
+				return true;
+			}else {
+				return false;
+			}
+		} catch (SQLException e) {
+			
+			App.erro.mensagemErro("Erro ao realizar login");
+		}	
+		return false;
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/View/Tela_Cadastros.java
@@ -0,0 +1,266 @@
+package View;
+
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.SwingConstants;
+
+import App.App;
+import Model.Cliente;
+import Model.Empregado;
+
+
+public class Tela_Cadastros extends JFrame implements ActionListener{
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	JLabel prolnome, prolvalidade, prolpreco, prolquant_estoque, proldescricao;
+	JTextField profnome, profvalidade, profpreco, profquant_estoque, profdescricao;
+	JButton probcadastrar, probsair, problimpar;
+
+	JLabel clilnome, cliltipo, cliltelefone;
+	JTextField clifnome, cliftelefone;
+	JButton clibcadastrar, clibsair, cliblimpar;
+	JComboBox clicombo;
+
+	JLabel selnome, seldescricao;
+	JTextField sefnome, sefdescricao;
+	JButton sebcadastrar, sebsair, seblimpar;
+	
+	JLabel fulnome, fulsexo, fulmatricula, fullogin, fulsenha, fulresenha;
+	JTextField fufnome, fufmatricula, fuflogin, fufsenha, fufresenha;
+	JButton fubcadastrar, fubsair, fublimpar;
+	
+	public Tela_Cadastros(){
+		super( "Tela de Cadastros" );
+		JTabbedPane tabbedPane = new JTabbedPane(); 
+
+		// TELA PRODUTOS 
+		prolnome = new JLabel("NOME:");
+		profnome = new JTextField(25);
+		prolvalidade = new JLabel("VALIDADE:");
+		profvalidade = new JTextField(25);
+		prolpreco = new JLabel("PREÇO:");
+		profpreco = new JTextField(25);
+		prolquant_estoque = new JLabel("QUANTIDADE EM ESTOQUE:");
+		profquant_estoque = new JTextField(25);
+		proldescricao= new JLabel("DESCRIÇÃO:");
+		profdescricao = new JTextField(25);
+		probcadastrar = new JButton("CADASTRAR");
+		probcadastrar.addActionListener(this);
+		probsair = new JButton("SAIR");
+		probsair.addActionListener(this);
+		problimpar = new JButton("LIMPAR");
+		problimpar.addActionListener(this);
+
+
+		JLabel label1 = new JLabel( "", SwingConstants.CENTER );
+		JPanel panel1 = new JPanel(); 
+
+		panel1.add(prolnome);
+		panel1.add(profnome);
+		panel1.add(prolvalidade);
+		panel1.add(profvalidade);
+		panel1.add(prolpreco);
+		panel1.add(profpreco);
+		panel1.add(prolquant_estoque);
+		panel1.add(profquant_estoque);
+		panel1.add(proldescricao);
+		panel1.add(profdescricao);
+		panel1.add(probcadastrar);
+		panel1.add(probsair);
+		panel1.add(problimpar);
+
+		panel1.add( label1 );
+		tabbedPane.addTab( "PRODUTO", null, panel1, "First Panel" ); 
+
+
+
+
+		// TELA CLIENTE
+
+		clicombo = new JComboBox(new Object[]{"              PESSOA FÍSICA           ", "          PESSOA JURÍDICA        "});
+		clilnome = new JLabel("               NOME:               ");
+		clifnome = new JTextField(25);
+		cliltipo = new JLabel("                    TIPO:                    ");
+		cliltelefone = new JLabel("TELEFONE:");
+		cliftelefone = new JTextField(25);
+		clibcadastrar = new JButton("CADASTRAR");
+		clibcadastrar.addActionListener(this);
+		clibsair = new JButton("SAIR");
+		clibsair.addActionListener(this);
+		cliblimpar = new JButton("LIMPAR");
+		cliblimpar.addActionListener(this);
+
+		JLabel label2 = new JLabel( "", SwingConstants.CENTER );
+		JPanel panel2 = new JPanel(); 
+
+		panel2.add(cliltipo);
+		panel2.add(clicombo);
+		panel2.add(clilnome);
+		panel2.add(clifnome);
+		panel2.add(cliltelefone);
+		panel2.add(cliftelefone);
+		panel2.add(clibcadastrar);
+		panel2.add(clibsair);
+		panel2.add(cliblimpar);
+
+		panel2.add( label2 ); 
+		tabbedPane.addTab( "CLIENTE", null, panel2, "Second Panel" );
+
+
+
+
+		// TELA SESSÃO
+
+		selnome = new JLabel("NOME:");
+		sefnome = new JTextField(25);
+		seldescricao = new JLabel("DESCRIÇÃO:");
+		sefdescricao = new JTextField(25);
+		sebcadastrar = new JButton("CADASTRAR");
+		sebcadastrar.addActionListener(this);
+		sebsair = new JButton("SAIR");
+		sebsair.addActionListener(this);
+		seblimpar = new JButton("LIMPAR");
+		seblimpar.addActionListener(this);
+
+		JLabel label3 = new JLabel( "", SwingConstants.CENTER );
+		JPanel panel3 = new JPanel(); 
+
+		panel3.add(selnome);
+		panel3.add(sefnome);
+		panel3.add(seldescricao);
+		panel3.add(sefdescricao);
+		panel3.add(sebcadastrar);
+		panel3.add(sebsair);
+		panel3.add(seblimpar);
+
+		tabbedPane.addTab( "SESSÃO", null, panel3, "Third Panel" );
+
+
+
+		//TELA FUNCIONÁRIO
+
+		JComboBox fucombo = new JComboBox(new Object[]{"               MASCULINO               ", "               FEMININO               "});
+
+		fulsexo = new JLabel("               SEXO:               ");
+		fulnome = new JLabel("               NOME:               ");
+		fufnome = new JTextField(25);
+		fulmatricula = new JLabel("MATRÍCULA:");
+		fufmatricula = new JTextField(25);
+		fullogin = new JLabel("LOGIN:");
+		fuflogin = new JTextField(25);
+		fulsenha = new JLabel("SENHA:");
+		fufsenha = new JTextField(25);
+		fulresenha = new JLabel("CONFIRMAR A SENHA:");
+		fufresenha = new JTextField(25);
+		fubcadastrar = new JButton("CADASTRAR");
+		fubcadastrar.addActionListener(this);
+		fubsair = new JButton("SAIR");
+		fubsair.addActionListener(this);
+		fublimpar = new JButton("LIMPAR");
+		fublimpar.addActionListener(this);
+
+		JLabel label4 = new JLabel( "", SwingConstants.CENTER );
+		JPanel panel4 = new JPanel(); 
+
+		panel4.add(fulsexo);
+		panel4.add(fucombo);
+		panel4.add(fulnome);
+		panel4.add(fufnome);
+		panel4.add(fulmatricula);
+		panel4.add(fufmatricula);
+		panel4.add(fullogin);
+		panel4.add(fuflogin);
+		panel4.add(fulsenha);
+		panel4.add(fufsenha);
+		panel4.add(fulresenha);
+		panel4.add(fufresenha);
+		panel4.add(fubcadastrar);
+		panel4.add(fubsair);
+		panel4.add(fublimpar);
+
+		panel4.add( label4 );
+		tabbedPane.addTab( "FUNCIONÁRIO", null, panel4, "Second Panel" );
+
+		add( tabbedPane );
+		
+	}
+
+		
+	public void actionPerformed(ActionEvent e) {
+		// AÇÃO PRODUTO
+		if(e.getSource().equals(this.probcadastrar)) {
+//			Empregado e  =  new Empregado(nome, tipo, telefone, matricula, senha);
+//			App.banco.gravarEmpregado(e);;
+			dispose();
+		}
+		if (e.getSource().equals(this.probsair)) {
+			dispose();
+			new Tela_Menu();
+		}
+		if (e.getSource().equals(this.problimpar)) {
+			dispose();
+			// COMANDO LIMPAR JTEXTFIELD
+		}
+		
+		//AÇÃO CLIENTE
+		if(e.getSource().equals(this.clibcadastrar)) {
+	//		Cliente c = new Cliente(nome, tipo, telefone);
+	//		APP.BD.cadastrarCliente(c);
+			
+			if(clicombo.getSelectedItem().equals("Pessoa Fisica")){
+//				int c = 0;
+			}
+			
+			dispose();
+		}
+		if (e.getSource().equals(this.clibsair)) {
+			dispose();
+			new Tela_Menu();
+		}
+		if (e.getSource().equals(this.cliblimpar)) {
+			dispose();
+			// COMANDO LIMPAR JTEXTFIELD
+		}
+		
+		//AÇÃO SESSÃO
+		if(e.getSource().equals(this.sebcadastrar)) {
+			//METODO CADASTRAR
+			dispose();
+		}
+		if (e.getSource().equals(this.sebsair)) {
+			dispose();
+			new Tela_Menu();
+		}
+		if (e.getSource().equals(this.seblimpar)) {
+			dispose();
+			// COMANDO LIMPAR JTEXTFIELD
+		}
+		
+		//AÇÃO FUNCIONÁRIO
+		if(e.getSource().equals(this.fubcadastrar)) {
+			//METODO CADASTRAR
+			dispose();
+		}
+		if (e.getSource().equals(this.fubsair)) {
+			dispose();
+			new Tela_Menu();
+		}
+		if (e.getSource().equals(this.fublimpar)) {
+			dispose();
+			// COMANDO LIMPAR JTEXTFIELD
+		}
+	}
+
+} 
+
