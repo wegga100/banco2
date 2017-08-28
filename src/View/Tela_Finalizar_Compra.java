9b2cf66e23c4dfd48b6c275666dd99dab3ed0d70
Vaniedson <vaniedsonsi@gmail.com>
2017-08-28 00:04:33 -0300

    XFG

---
+++ src/View/Tela_Finalizar_Compra.java
@@ -0,0 +1,21 @@
+package View;
+
+import javax.swing.JFrame;
+
+public class Tela_Finalizar_Compra extends JFrame{
+	
+	public Tela_Finalizar_Compra() {
+		
+		setSize(300,400);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setResizable(false);
+		setVisible(true);
+	
+		
+	}
+	public static void main(String[] args) {
+		new Tela_Finalizar_Compra();
+	}
+
+}
